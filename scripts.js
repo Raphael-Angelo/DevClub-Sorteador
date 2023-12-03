ec2e2d92b043e656197adc6f1a503f698eaae4c3
Raphael-Angelo <avogatofps@gmail.com>
2023-12-02 23:31:46 -0300

    add MinMax from scripts.js

--- scripts.js
+++ scripts.js
@@ -2,8 +2,13 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if(min >= max) {
+        alert("O valor minimo deve ser MENOR que o valor MÁXIMO")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert (result)
+        alert (result)
+    
+    }
 
 }
